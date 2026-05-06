c9b992eb9a0f1d07b727b0c097fa6a3b4d801937
Edwin Kesuma <edwin.kesuma.EK21@gmail.com>
2026-05-06 19:34:44 +0700

    Initial commit

---
+++ src/test/java/com/edwin_kesuma/Neon/NeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin_kesuma.Neon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
